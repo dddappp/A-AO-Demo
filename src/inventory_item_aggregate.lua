46e13839e0d66921e18b053a5fbe49396b52f79f
yangjiefeng <yangjiefeng@gmail.com>
2024-05-30 17:43:00 +0800

    regenerate code

--- src/inventory_item_aggregate.lua
+++ src/inventory_item_aggregate.lua
@@ -16,51 +16,37 @@ inventory_item_aggregate.ERRORS = ERRORS
 
 local inventory_item_table
 
-function inventory_item_aggregate.init(t)
-    inventory_item_table = t
+function inventory_item_aggregate.init(table)
+    inventory_item_table = table
 end
 
--- function inventory_item_aggregate.get_inventory_item(_inventory_item_id)
---     return entity_coll.get(InventoryItemTable, json.encode(inventory_item_id.to_key_array(_inventory_item_id)))
--- end
-
 function inventory_item_aggregate.add_inventory_item_entry(cmd, msg, env)
-    --
-    -- Create new instances on demand
-    --
-
-    -- local _inventory_item_id = inventory_item_id.new(
-    --     cmd.product_id,
-    --     cmd.location,
-    --     cmd.inventory_attribute_set
-    -- )
     local _inventory_item_id = cmd.inventory_item_id
-    local inventory_item_id_key = json.encode(inventory_item_id.to_key_array(_inventory_item_id))
-    local state = entity_coll.get_copy_or_else_nil(inventory_item_table, inventory_item_id_key)
-    -- local state = entity_coll.get(article_table, cmd.article_id)
-    if (state and state.version ~= cmd.version) then
+    local _inventory_item_id_key = json.encode(inventory_item_id.to_key_array(_inventory_item_id))
+    local _state = entity_coll.get_copy_or_else_nil(inventory_item_table, _inventory_item_id_key)
+    if (_state and _state.version ~= cmd.version) then
         error(ERRORS.CONCURRENCY_CONFLICT)
     end
-    -- local _inventory_item_id = state.inventory_item_id
-    if (state) then
-        state.inventory_item_id = _inventory_item_id
+    if (_state) then
+        _state.inventory_item_id = _inventory_item_id
     end
-    local version = state and state.version or nil
-    local event = inventory_item_add_inventory_item_entry_logic.verify(state, _inventory_item_id, cmd.movement_quantity,
+    local version = _state and _state.version or nil
+    local _event = inventory_item_add_inventory_item_entry_logic.verify(_state, _inventory_item_id, cmd.movement_quantity,
         cmd, msg, env)
-    if (event.inventory_item_id ~= _inventory_item_id) then
+    if (_event.inventory_item_id ~= _inventory_item_id) then
         error(ERRORS.ENTITY_ID_MISMATCH)
     end
-    if (version and (event.version ~= version)) then
+    if (version and (_event.version ~= version)) then
         error(ERRORS.VERSION_MISMATCH)
     end
-    local new_state = inventory_item_add_inventory_item_entry_logic.mutate(state, event, msg, env)
-    new_state.inventory_item_id = _inventory_item_id
-    new_state.version = version and (version + 1) or 0
+    local _new_state = inventory_item_add_inventory_item_entry_logic.mutate(_state, _event, msg, env)
+    _new_state.inventory_item_id = _inventory_item_id
+    _new_state.version = version and (version + 1) or 0
     local commit = function()
-        entity_coll.add_or_update(inventory_item_table, inventory_item_id_key, new_state)
+        entity_coll.add_or_update(inventory_item_table, _inventory_item_id_key, _new_state)
     end
-    return event, commit
+
+    return _event, commit
 end
 
 return inventory_item_aggregate
