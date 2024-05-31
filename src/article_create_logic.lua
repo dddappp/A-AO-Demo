17bc640579c9f5f68425f8890aeb0f3ae167346e
yangjiefeng <yangjiefeng@gmail.com>
2024-05-31 12:25:26 +0800

    regenerate code

--- src/article_create_logic.lua
+++ src/article_create_logic.lua
@@ -2,12 +2,12 @@ local article = require("article")
 
 local article_create_logic = {}
 
+
 function article_create_logic.verify(article_id, title, body, owner, cmd, msg, env)
     return article.new_article_created(article_id, title, body, owner)
 end
 
 function article_create_logic.new(event, msg, env)
-    -- print("event.title" .. tostring(event.title))
     return article.new(event.title, event.body, event.owner)
 end
 
