4acaa9a6ffff134480cf90976ec58f068d20cc71
yangjiefeng <yangjiefeng@gmail.com>
2024-09-26 12:59:15 +0800

    update demo code

--- src/article_add_comment_logic.lua
+++ src/article_add_comment_logic.lua
@@ -3,8 +3,9 @@ local article = require("article")
 local article_add_comment_logic = {}
 
 
-function article_add_comment_logic.verify(_state, comment_seq_id, commenter, body, cmd, msg, env)
-    return article.new_comment_added(_state, comment_seq_id, commenter, body)
+function article_add_comment_logic.verify(_state, commenter, body, cmd, msg, env)
+    _state.comment_seq_id_generator = (_state.comment_seq_id_generator or 0) + 1
+    return article.new_comment_added(_state, _state.comment_seq_id_generator, commenter, body)
 end
 
 function article_add_comment_logic.mutate(state, event, msg, env)
