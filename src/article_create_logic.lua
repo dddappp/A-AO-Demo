c03fff6855e39303ae9473d26f64ea3f4e883404
yangjiefeng <yangjiefeng@gmail.com>
2024-09-28 15:37:05 +0800

    regenerate code

--- src/article_create_logic.lua
+++ src/article_create_logic.lua
@@ -2,24 +2,24 @@ local article = require("article")
 
 local article_create_logic = {}
 
---- Verify article create command
--- @param article_id number The unique identifier for the article
--- @param title string The title of the article
--- @param body string The content body of the article
--- @param owner string The owner of the article
+
+--- Verify Article.Create command
+-- @param article_id number The ArticleId of the Article
+-- @param title string 
+-- @param body string 
 -- @param cmd table The command
 -- @param msg any The original message
--- @param env any The environment context
+-- @param env table The environment context
 -- @return table The event, can use `article.new_article_created` to create it
 function article_create_logic.verify(article_id, title, body, cmd, msg, env)
     return article.new_article_created(article_id, title, body, msg.Owner)
 end
 
---- Create a new article
+--- Create a new Article
 -- @param event table The event
 -- @param msg any The original message
 -- @param env any The environment context
--- @return table The state of the article
+-- @return table The state of the Article
 function article_create_logic.new(event, msg, env)
     return article.new(event.title, event.body, event.owner)
 end
